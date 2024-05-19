74564dfd338b563eb015740b7e96952455ae78cc
Hsiu Ting <mr.hsiuting@gmail.com>
2024-05-19 01:51:03 -0700

    Add codes

---
+++ app.js
@@ -0,0 +1,40 @@
+let uploadedImage = null;
+
+document
+  .getElementById("imageUpload")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        uploadedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+// Replace with your landing page URL
+const reviewLandingPageUrl = "./";
+function generateQRCode() {
+  let qrCode = new QRCodeStyling({
+    width: 300,
+    height: 300,
+    data: reviewLandingPageUrl,
+    // Optional: Replace with your logo URL
+    image: uploadedImage,
+    dotsOptions: {
+      color: "#000000",
+      type: "rounded",
+    },
+    backgroundOptions: {
+      color: "#ffffff",
+    },
+    imageOptions: {
+      crossOrigin: "anonymous",
+      margin: 0,
+    },
+  });
+
+  // Clear previous QR code if it exists
+  document.getElementById("qrcode").innerHTML = "";
+  qrCode.append(document.getElementById("qrcode"));
+}
